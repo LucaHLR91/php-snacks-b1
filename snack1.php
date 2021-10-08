5f75fedc9c692581f26c35c383bdc95199f7a9f5
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-08 14:34:15 +0200

    Matches array

--- snack1.php
+++ snack1.php
@@ -4,3 +4,32 @@ Ogni array avrà una squadra di casa e una squadra ospite,
 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+    $matches = [
+        [
+            'homeTeam' => 'Olimpia Milano',
+            'awayTeam' => 'Cantù',
+            'homeScore' => 55,
+            'awayScore' => 60,
+        ],
+        [
+            'homeTeam' => 'Virtus Segafredo Bologna',
+            'awayTeam' => 'Fortitudo Pallacanestro Bologna',
+            'homeScore' => 80,
+            'awayScore' => 55,
+        ],
+        [
+            'homeTeam' => 'Reyer Venezia Mestre',
+            'awayTeam' => 'New Basket Brindisi',
+            'homeScore' => 50,
+            'awayScore' => 51,
+        ],
+        [
+            'homeTeam' => 'Pallacanestro Varese',
+            'awayTeam' => 'Victoria Libertas Pallacanestro',
+            'homeScore' => 35,
+            'awayScore' => 75,
+        ],
+    ];
+?>
