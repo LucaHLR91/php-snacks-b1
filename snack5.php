60fae6188e7725d15f5bd4fc471f1bc8763814dd
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-12 11:33:32 +0200

    Snack 6

--- snack5.php
+++ snack5.php
@@ -2,9 +2,9 @@
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
 
 <?php 
-    $paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa facere, deserunt esse voluptatum rerum sed odio cumque quisquam quasi iusto fugit nisi ullam! Delectus odio ab error, repellat sint blanditiis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias ipsam hic, eveniet laudantium nam, dolorem fuga nobis doloribus, facilis vel architecto? Ipsum architecto facere hic, tempora eveniet vel alias debitis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione totam, veniam quod, aperiam fugiat dignissimos amet voluptates architecto asperiores debitis rem voluptate sint eos natus sed omnis officia, facere modi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam dolor error perferendis sapiente tempore eos reprehenderit illo? Corporis, explicabo perspiciatis natus quae quia totam sunt voluptates minus provident voluptatibus minima? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis officia tempore hic quia! Impedit rem pariatur, fugiat quia dolorum veritatis enim iusto quisquam quasi, earum cum, recusandae corrupti molestias officia!';
+    $text = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa facere, deserunt esse voluptatum rerum sed odio cumque quisquam quasi iusto fugit nisi ullam! Delectus odio ab error, repellat sint blanditiis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias ipsam hic, eveniet laudantium nam, dolorem fuga nobis doloribus, facilis vel architecto? Ipsum architecto facere hic, tempora eveniet vel alias debitis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione totam, veniam quod, aperiam fugiat dignissimos amet voluptates architecto asperiores debitis rem voluptate sint eos natus sed omnis officia, facere modi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam dolor error perferendis sapiente tempore eos reprehenderit illo? Corporis, explicabo perspiciatis natus quae quia totam sunt voluptates minus provident voluptatibus minima? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis officia tempore hic quia! Impedit rem pariatur, fugiat quia dolorum veritatis enim iusto quisquam quasi, earum cum, recusandae corrupti molestias officia!';
 
-    $array_paragraph = explode('.', $paragraph);
+    $array_paragraph = explode('.', $text);
     var_dump($array_paragraph);
 ?>
 
@@ -18,8 +18,8 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il pa
 </head>
 <body>
     <?php 
-        foreach ($array_paragraph as $item) {
-            echo '<p>' . $item . '</p>';
+        foreach ($array_paragraph as $paragraph) {
+            echo '<p>' . $paragraph . '</p>';
         }
     ?>
 </body>
---
+++ snack6.php
@@ -0,0 +1,5 @@
+<!-- Stampiamo poi il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+<?php 
+    require __DIR__ . '/database/db_snack6.php';
+?>
