9ad9cd402f84aaba7de995e663408d79f91f6744
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-14 12:42:31 +0200

    Snack8

--- snack8.php
+++ snack8.php
@@ -1,2 +1,15 @@
 <!-- Ad ogni refresh della pagina visualizzare una pubblicità a schermo, tenendo conto che possono essere sorteggiate solo quelle is_active true. -->
 
+<?php 
+    require __DIR__ . '/database/db_snack8.php';
+
+    function displayrndPhoto($ads) {
+        $rndNumb = rand(0, count($ads) - 1);
+        if($ads[$rndNumb]['is_active']) {
+            echo '<img src="' . $ads[$rndNumb]['image_path'] . '" alt = "foto">';
+        }
+        
+    }
+
+    $img = displayrndPhoto($ads);
+?>
