2ac771ce1256f4597a375cc3200f99545eaea791
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-15 12:18:33 +0200

    Snack9A

---
+++ snack9A.php
@@ -0,0 +1,28 @@
+<?php 
+    require __DIR__ . '/database/db_snack9A.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack9A</title>
+</head>
+<body>
+    <?php 
+        foreach ($hotels as $hotel) {
+    ?>
+        <div class="hotel">
+            <h2><?php echo $hotel['name'];?></h2>
+            <p><?php echo $hotel['description'];?></p>
+            <p>Parking: <?php echo $hotel['parking'];?></p>
+            <p>Vote: <?php echo $hotel['vote'];?></p>
+            <p>Distance to center: <?php echo $hotel['distance_to_center'];?> km</p>
+        </div>
+    <?php
+    };
+    ?>
+</body>
+</html>
