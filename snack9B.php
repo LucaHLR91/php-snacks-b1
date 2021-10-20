482c12a291c96805de4bf38f7e2ea690b38f4cac
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-20 12:37:44 +0200

    Snack9B

---
+++ snack9B.php
@@ -0,0 +1,20 @@
+<!-- Attraverso un parametro GET da inserire direttamente nell’url (uno alla volta), filtrare gli hotel che hanno un parcheggio, numero minimo di stelle o massima lontananza dal centro.
+Se non c’è un filtro, visualizzare come in precedenza tutti gli hotel -->
+
+<?php 
+    require __DIR__ . '/database/db_snack9B.php';
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack9B</title>
+</head>
+<body>
+          
+</body>
+</html>
